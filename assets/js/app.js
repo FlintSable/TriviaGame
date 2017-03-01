6ab7435fce50f2d531bb7f847835eaf34eac73e2
unknown <nicholasn@WS777.foga.net>
2017-02-28 17:24:27 -0800

    building object with different data

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+var triviaJS = {
+	questions:{
+
+		qOne:{
+			dataOne:"wumbo",
+			dataTwo:"wumtwo",
+			dataThree:"wumThree",
+
+		},
+		qTwo:{
+
+		},
+		qThree:{
+
+		},
+		qFour:{
+
+		},
+		qFive:{
+
+		},
+		qSix:{
+
+		},
+		qSeven:{
+
+		},
+		qEight:{
+
+		},
+		qNine:{
+
+		},
+		qTen:{
+
+		}
+	},
+
+
+
+
+
+
+
+
+
+
+
+
+}
