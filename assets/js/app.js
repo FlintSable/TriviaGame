31b79877a2332196a90cb4c44b69d1400fc52e01
Nicholas N <nnlaor@gmail.com>
2017-03-12 16:26:58 -0700

    updating switch statement

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,103 +2,119 @@ var triviaJS = {
 	// object: values inside
 	questions:{
 
-		questOne:{
+		quest1:{
 			q1:"Dear reader, sorry this is not complete",
 			choices:{
-				a:"the a",
+				a:"the a-",
 				b:"the b",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 65
 
 		},
-		questTwo:{
+		quest2:{
 			q1:"The timer for the questions are active",
 			choices:{
 				a:"the a",
-				b:"the b",
+				b:"the b-",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 66
+
 		},
-		questThree:{
+		quest3:{
 			q1:"If you get to this before I fix it, sorry",
 			choices:{
-				a:"the a",
+				a:"the a-",
 				b:"the b",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 65
 		},
-		questFour:{
+		quest4:{
 			q1:"I think I could get this fully functional",
 			choices:{
 				a:"the a",
 				b:"the b",
-				c:"the c",
+				c:"the c-",
 				d:"the d"
-
-			}
+			},
+			ans: 67
 		},
-		questFive:{
+		quest5:{
 			q1:"might just need some time",
 			choices:{
 				a:"the a",
 				b:"the b",
-				c:"the c",
+				c:"the c-",
 				d:"the d"
-			}
+			},
+			ans: 67
+
 		},
-		questSix:{
+		quest6:{
 			q1:"there are some major parts missing",
 			choices:{
 				a:"the a",
 				b:"the b",
 				c:"the c",
-				d:"the d"
-			}
+				d:"the d-"
+			},
+			ans: 68
+
 		},
-		questSeven:{
+		quest7:{
 			q1:"I added color while you wait",
 			choices:{
 				a:"the a",
 				b:"the b",
-				c:"the c",
+				c:"the c-",
 				d:"the d"
-			}
+			},
+			ans: 67
+
 		},
-		questEight:{
+		quest8:{
 			q1:"thank you for reading",
 			choices:{
-				a:"the a",
+				a:"the a-",
 				b:"the b",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 65
+
 		},
-		questNine:{
+		quest9:{
 			q1:"hope you have a good night",
 			choices:{
 				a:"the a",
-				b:"the b",
+				b:"the b-",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 66
+
 		},
-		questTen:{
+		quest10:{
 			q1:"I need to make more",
 			choices:{
-				a:"the a",
+				a:"the a-",
 				b:"the b",
 				c:"the c",
 				d:"the d"
-			}
+			},
+			ans: 65
 		}
 
 	},
 
 	timeValue: 7,
 	questionCount: "questOne",
+	questCount: 0,
 
 
 
@@ -109,10 +125,10 @@ var triviaJS = {
 			$clearLayer = $('<div id="clearLayer"></div>');
 			$qModal = $('<div id="qModal">');
 			$aObj = $('<div id="aObj">');
-			$aItem = $('<button id="aItems">' + ansObj.a + '</button>');
-			$bItem = $('<button id="aItems">' + ansObj.b + '</button>');
-			$cItem = $('<button id="aItems">' + ansObj.c + '</button>');
-			$dItem = $('<button id="aItems">' + ansObj.d + '</button>');
+			$aItem = $('<button id="aItems" data-value=65>' + ansObj.a + '</button>');
+			$bItem = $('<button id="bItems" data-value=66>' + ansObj.b + '</button>');
+			$cItem = $('<button id="cItems" data-value=67>' + ansObj.c + '</button>');
+			$dItem = $('<button id="dItems" data-value=68>' + ansObj.d + '</button>');
 			$cont1 = $('<div id="cont1">');
 			$cont2 = $('<div id="cont2">');
 			$actualQuestion = $('<div id="actualQuestion loader">' + question + '</div>');
@@ -126,49 +142,102 @@ var triviaJS = {
 
 
 			$('body').append( $ghost);
-			// $loader,
+			$("button").on("click", function(){
+				console.log($(this).attr('data-value'));
+				console.log(question);
 
+				if($(this).attr('data-value') == this.ans){
+					console.log('CORRECT');
+				}
+				else{
+					console.log('INCORRECT')
+				}
+
+			});
 	},
 		
 
 	startTimer: function(q1, a1){
-		// var questionCount = 0;
-		// console.log(q1Obj);
-		// var loopObject = function(q1Obj){
-		// 	completeProcess(q1Obj[questionCount], function(){
-		// 		questionCount++;
-		// 		console.log(questionCount);
-		// 		if(questionCount<q1Obj.length){
-		// 			loopObject(q1Obj);
-		// 		}
-		// 	});
-		// }
-		// function completeProcess(log, callback){
-		// 	console.log(log);
 
-		// 	intervalTime = setInterval(this.decrement, 1000);
-		// 	triviaJS.modalRender(q1Obj);
-		// 	callback();
-		// }
-		// loopObject(q1Obj);
-
-			// for(var key in triviaJS.questions){
-			// 	console.log(triviaJS.questions[key]);
-			// 	console.log(questionCount);
-				console.log(triviaJS.questions.questOne.choices);
 				intervalTime = setInterval(this.decrement, 1000);
 				this.modalRender(q1, a1);
-			// }
 
+	
 			
 	},
 
 	decrement: function(){	
 			triviaJS.timeValue--;
+
 			// $("#show-time").html("<h2>" + triviaJS.timeValue + "</h2>");
 			$showTime = $('<div id="showTime"></div>');
 			$cont1.append($showTime);
 			$("#showTime").html(triviaJS.timeValue);
+
+			function stateEvaluate(questCount){
+
+
+				switch (triviaJS.questCount){
+					case 0:
+					triviaJS.startTimer(triviaJS.questions.quest1.q1, triviaJS.questions.quest1.choices);
+					console.log('in the sqitch ' + questCount);
+					break;
+
+					case 1:
+					triviaJS.startTimer(triviaJS.questions.quest2.q1, triviaJS.questions.quest2.choices);
+					console.log(questCount);
+					break;
+
+
+					case 2:
+					triviaJS.startTimer(triviaJS.questions.quest3.q1, triviaJS.questions.quest3.choices);
+					console.log(questCount);
+					break;
+
+					case 3:
+					triviaJS.startTimer(triviaJS.questions.quest4.q1, triviaJS.questions.quest4.choices);
+					console.log(questCount);
+					break;
+
+					case 4:
+					triviaJS.startTimer(triviaJS.questions.quest5.q1, triviaJS.questions.quest5.choices);
+					console.log(questCount);
+					break;
+
+					case 5:
+					triviaJS.startTimer(triviaJS.questions.quest6.q1, triviaJS.questions.quest6.choices);
+					console.log(questCount);
+					break;
+
+					case 6:
+					triviaJS.startTimer(triviaJS.questions.quest7.q1, triviaJS.questions.quest7.choices);
+					console.log(questCount);
+					break;
+
+					case 7:
+					triviaJS.startTimer(triviaJS.questions.quest8.q1, triviaJS.questions.quest8.choices);
+					console.log(questCount);
+					break;
+
+					case 8:
+					triviaJS.startTimer(triviaJS.questions.quest9.q1, triviaJS.questions.quest9.choices);
+					console.log(questCount);
+					break;
+
+					case 9:
+					triviaJS.startTimer(triviaJS.questions.quest10.q1, triviaJS.questions.quest10.choices);
+					console.log(questCount);
+					break;
+
+					case 10:
+					console.log('score!!!s');
+					break;
+
+
+				}
+			};
+
+
 
 			if(triviaJS.timeValue <= 0){
 				// fucntion to start modal
@@ -177,60 +246,11 @@ var triviaJS = {
 				$('#ghost').remove();
 				triviaJS.timeValue = 5;
 
-				setTimeout(function(){ // here you can run another function which 
-					// will run the start timer based on some vairable 
-					// that has been set
-					if(triviaJS.questionCount ==  'questOne'){
-						triviaJS.questions.questOne.choices
-						triviaJS.startTimer(triviaJS.questions.questTwo.q1, triviaJS.questions.questTwo.choices);
+				setTimeout(function(){ 
+					triviaJS.questCount++;
+					console.log('timeout function'+triviaJS.questCount);
+					stateEvaluate(triviaJS.questCount);
 
-						triviaJS.questionCount = 'questTwo';
-	
-
-					 }else if(triviaJS.questionCount == 'questTwo'){
-						triviaJS.startTimer(triviaJS.questions.questThree.q1, triviaJS.questions.questThree.choices);
-						triviaJS.questionCount = 'questThree';
-
-
-					 }else if(triviaJS.questionCount == 'questThree'){
-					 	triviaJS.startTimer(triviaJS.questions.questFour.q1, triviaJS.questions.questFour.choices);
-						triviaJS.questionCount = 'questFour';
-
-
-					 }else if(triviaJS.questionCount == 'questFour'){
-					 	triviaJS.startTimer(triviaJS.questions.questFive.q1, triviaJS.questions.questFive.choices);
-					 	triviaJS.questionCount = 'questFive';
-
-
-					 }else if(triviaJS.questionCount == 'questFive'){
-					 	triviaJS.startTimer(triviaJS.questions.questSix.q1, triviaJS.questions.questSix.choices);
-					 	triviaJS.questionCount = 'questSix';
-
-
-					 }else if(triviaJS.questionCount == 'questSix'){
-					 	triviaJS.startTimer(triviaJS.questions.questSeven.q1, triviaJS.questions.questSeven.choices);
-					 	triviaJS.questionCount = 'questSeven';
-
-
-					 }else if(triviaJS.questionCount == 'questSeven'){
-					 	triviaJS.startTimer(triviaJS.questions.questEight.q1, triviaJS.questions.questEight.choices);
-					 	triviaJS.questionCount = 'questEight';
-
-
-					 }else if(triviaJS.questionCount == 'questEight'){
-					 	triviaJS.startTimer(triviaJS.questions.questNine.q1, triviaJS.questions.questNine.choices);
-						triviaJS.questionCount = 'questNine';
-
-
-					 }else if(triviaJS.questionCount == 'questNine'){
-					 	triviaJS.startTimer(triviaJS.questions.questTen.q1, triviaJS.questions.questTen.choices);
-					 	triviaJS.questCount = 'summary';
-
-
-					 }else if(triviaJS.questionCount == 'summary'){
-					 	console.log("total score")
-					 }
-					// triviaJS.startTimer("object list loop");
 				}, 4000);
 
 
@@ -240,42 +260,25 @@ var triviaJS = {
 
 
 	},
-	stop: function(){},
 
-	startQuiz: function(){
-			// display modal question
-		this.displayModalQuestion
-			// start the timer
-			// once times up rerun
 
-	},
 
-	displayModalQuestion: function(){},
+
+
 
 
 
 }
 
-// for (i=; i<5; i++){
 
-// };
-
-triviaJS.startTimer(triviaJS.questions.questOne.q1, triviaJS.questions.questOne.choices);
-// triviaJS.startTimer(triviaJS.questions);
+triviaJS.startTimer(triviaJS.questions.quest1.q1, triviaJS.questions.quest1.choices);
 
 window.onload = function(){
-	// if any of these are clicked add to score
-	// if anything is clicked get the triviaJS.questionCount and rerun the start timer 
+
 	triviaJS.decrement();
 
-	// else add to incorrects
-	// anything is clicked get the triviaJS.questionCount  and rerun the start timer accoudingly
+
+
 
 };
 
-
-
-// needs welcome button to start the modals
-// needs to restart the modals with object questions
-// check for answers
-// if answer is correct then add to points
